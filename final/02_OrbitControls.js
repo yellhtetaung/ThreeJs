a23286075bff25c98481af87540adafb54e9a615
yellhtetaung <yehtetaung.dev@gmail.com>
2023-11-11 14:58:11 +0630

    init

--- final/02_OrbitControls.js
+++ final/02_OrbitControls.js
@@ -21,38 +21,13 @@ const orbit = new OrbitControls(camera, renderer.domElement);
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
-camera.position.set(-10, 30, 30);
+camera.position.set(0, 2, 5);
 orbit.update();
 
 const boxGeometry = new THREE.BoxGeometry();
 const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
-
-const planeGeometry = new THREE.PlaneGeometry(30, 30);
-const planeMaterial = new THREE.MeshBasicMaterial({
-	color: 0xffffff,
-	side: THREE.DoubleSide,
-});
-const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-scene.add(plane);
-plane.rotation.x = -0.5 * Math.PI;
-
-const gridHelper = new THREE.GridHelper(30);
-scene.add(gridHelper);
-
-const sphereGeometry = new THREE.SphereGeometry(4, 50, 50);
-const sphereMaterial = new THREE.MeshBasicMaterial({
-	color: 0x0000ff,
-	wireframe: false,
-});
-// const sphereMaterial = new THREE.MeshLambertMaterial({
-// 	color: 0x0000ff,
-// 	wireframe: true,
-// });
-const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-scene.add(sphere);
-sphere.position.set(-10, 10, 0);
 
 function animate(time) {
 	box.rotation.x = time / 1000;
